1db58963247eaf263e5af940acadabaca8ecb29d
Todd Hainsworth <hainsworth.todd@gmail.com>
2022-08-23 21:31:03 +0930

    prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home/Home"
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,18 @@
-import { Button } from 'flowbite-react';
-import React, { useState } from 'react';
+import { Button } from "flowbite-react";
+import React, { useState } from "react";
 
 const Home = () => {
-	const [clicks, setClicks] = useState(0);
+  const [clicks, setClicks] = useState(0);
 
-	return (
-		<div className="home">
-			<h1 className="text-primary text-3xl font-bold">React is up and running</h1>
-			<p>You've clicked {clicks} times</p>
-			<Button onClick={() => setClicks(clicks + 1)}>Click me!</Button>
-		</div>
-	);
-}
+  return (
+    <div className="home">
+      <h1 className="text-primary text-3xl font-bold">
+        React is up and running
+      </h1>
+      <p>You've clicked {clicks} times</p>
+      <Button onClick={() => setClicks(clicks + 1)}>Click me!</Button>
+    </div>
+  );
+};
 
 export default Home;
