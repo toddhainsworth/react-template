563b262ac45e05b9be61dbadd6aa96b6d4111abc
Todd Hainsworth <hainsworth.todd@gmail.com>
2022-08-23 20:57:57 +0930

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>React is up and running</h1>;
+  return <h1 className="text-primary text-3xl font-bold">React is up and running</h1>;
 };
 
 export default App;
