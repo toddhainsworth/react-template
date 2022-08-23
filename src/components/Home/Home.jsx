316bf0b44cd9472f982b3ebf20d41314c3d30d32
Todd Hainsworth <hainsworth.todd@gmail.com>
2022-08-23 21:18:23 +0930

    add tailwind + flowbite

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Button } from 'flowbite-react';
+import React, { useState } from 'react';
 
 const Home = () => {
-	return <h1 className="text-primary text-3xl font-bold">React is up and running</h1>;
+	const [clicks, setClicks] = useState(0);
+
+	return (
+		<div className="home">
+			<h1 className="text-primary text-3xl font-bold">React is up and running</h1>
+			<p>You've clicked {clicks} times</p>
+			<Button onClick={() => setClicks(clicks + 1)}>Click me!</Button>
+		</div>
+	);
 }
 
 export default Home;
